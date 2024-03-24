d06869a83889c20c3765a3be34d04da732735fc1
KomTudo <129621963+KomTudo@users.noreply.github.com>
2024-03-24 20:12:38 +0000

    Version 1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//write your code here
+function generateExcuse() {
+  var who = ["The dog", "My grandma", "The mailman", "My bird"];
+  var action = ["ate", "peed", "crushed", "broke"];
+  var what = ["my homework", "my phone", "the car"];
+  var when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  let excuse =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+
+  return excuse;
+}
+
+function setExcuse() {
+  let excuseElement = document.getElementById("newExcuse");
+  excuseElement.innerHTML = generateExcuse();
+}
+
+window.onload = setExcuse();
